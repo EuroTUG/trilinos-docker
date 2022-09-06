26f73c1db33b9140e1e6d0061a5daceaf5689d74
Matthias Mayr <matthias.mayr@unibw.de>
2022-09-06 22:48:22 +0200

    First working state of ex_03_solve

--- exercises/ex_03_solve/src/main.cpp
+++ exercises/ex_03_solve/src/main.cpp
@@ -4,9 +4,16 @@
 
 #include "utils.hpp"
 
-#include <Belos_LinearProblem.hpp>
-#include <Belos_MultiVecTraits.hpp>
-#include <Belos_OperatorTraits.hpp>
+#include <BelosBlockCGSolMgr.hpp>
+#include <BelosBlockGmresSolMgr.hpp>
+#include <BelosConfigDefs.hpp>
+#include <BelosLinearProblem.hpp>
+#include <BelosMultiVecTraits.hpp>
+#include <BelosOperatorTraits.hpp>
+#include <BelosSolverFactory.hpp>
+#include <BelosSolverManager.hpp>
+#include <BelosTpetraAdapter.hpp>
+#include <BelosTypes.hpp>
 
 #include <Teuchos_Array.hpp>
 #include <Teuchos_RCP.hpp>
@@ -16,6 +23,7 @@
 #include <Tpetra_CrsMatrix.hpp>
 #include <Tpetra_Map.hpp>
 #include <Tpetra_MultiVector.hpp>
+#include <Tpetra_Operator.hpp>
 #include <Tpetra_Vector.hpp>
 #include <Tpetra_Version.hpp>
 
@@ -24,15 +32,20 @@ int main(int argc, char *argv[]) {
   using Teuchos::rcp;
   using Teuchos::tuple;
 
-  using crs_matrix_type = Tpetra::CrsMatrix<>;
-  using map_type = Tpetra::Map<>;
-  using multivec_type = Tpetra::MultiVector<>;
-  using operator_type = Tpetra::Operator<>;
-  using vec_type = Tpetra::Vector<>;
+  using scalar_type = Tpetra::MultiVector<>::scalar_type;
+  using local_ordinal_type = Tpetra::MultiVector<>::local_ordinal_type;
+  using global_ordinal_type = Tpetra::MultiVector<>::global_ordinal_type;
+  using node_type = Tpetra::MultiVector<>::node_type;
 
-  using scalar_type = multivec_type::scalar_type;
-  using local_ordinal_type = multivec_type::local_ordinal_type;
-  using global_ordinal_type = multivec_type::global_ordinal_type;
+  using crs_matrix_type = Tpetra::CrsMatrix<scalar_type, local_ordinal_type, global_ordinal_type, node_type>;
+  using map_type = Tpetra::Map<local_ordinal_type, global_ordinal_type, node_type>;
+  using multivec_type = Tpetra::MultiVector<scalar_type, local_ordinal_type, global_ordinal_type, node_type>;
+  using operator_type = Tpetra::Operator<scalar_type, local_ordinal_type, global_ordinal_type, node_type>;
+  using vec_type = Tpetra::Vector<scalar_type, local_ordinal_type, global_ordinal_type, node_type>;
+
+  // using scalar_type = multivec_type::scalar_type;
+  // using local_ordinal_type = multivec_type::local_ordinal_type;
+  // using global_ordinal_type = multivec_type::global_ordinal_type;
 
   typedef Belos::OperatorTraits<scalar_type,multivec_type,operator_type> OPT;
   typedef Belos::MultiVecTraits<scalar_type,multivec_type> MVT;
@@ -42,7 +55,7 @@ int main(int argc, char *argv[]) {
   Tpetra::global_size_t numGblIndices = 50; clp.setOption("n", &numGblIndices, "number of nodes / number of global indices (default: 50)");
   std::string matrixType = "Laplace2D"; clp.setOption("matrixType", &matrixType, "Type of problem to be solved [Laplace1D, Laplace2D, Laplace3D, Elasticity2D, Elasticity3D] (default: Laplace2D)");
   scalar_type tol = 1.0e-4; clp.setOption("tol", &tol, "Tolerance to check for convergence of Krylov solver");
-  int maxIters = -1; clp.setOption("maxIters", &maxIters, "Maximum number of iterations of the Krylov solver")
+  int maxIters = -1; clp.setOption("maxIters", &maxIters, "Maximum number of iterations of the Krylov solver");
 
   switch (clp.parse(argc, argv)) {
     case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:        return EXIT_SUCCESS;
@@ -62,10 +75,12 @@ int main(int argc, char *argv[]) {
     const size_t numProcs = comm->getSize();
 
     // Create an output stream
+    RCP<Teuchos::FancyOStream> allOut = Teuchos::fancyOStream(Teuchos::rcpFromRef(std::cout));
     RCP<Teuchos::FancyOStream> out = Teuchos::fancyOStream(Teuchos::rcpFromRef(std::cout));
     out->setOutputToRootOnly(0);
 
     // Define some useful scalar values
+    const scalar_type zero = Teuchos::ScalarTraits<scalar_type>::zero();
     const scalar_type one = Teuchos::ScalarTraits<scalar_type>::one();
 
     ////////////////////////////////////////////////////////////////////////////
@@ -73,71 +88,88 @@ int main(int argc, char *argv[]) {
     *out << "\n>> I. Create the system matrix for a " << matrixType << " problem.\n" << std::endl;
 
     Teuchos::ParameterList galeriList;
+    int nx, ny, nz;
+    nx = ny = nz = 10;
     galeriList.set("nx", Teuchos::as<global_ordinal_type>(nx));
     galeriList.set("ny", Teuchos::as<global_ordinal_type>(ny));
     galeriList.set("nz", Teuchos::as<global_ordinal_type>(nz));
     galeriList.set("matrixType", matrixType);
     RCP<const crs_matrix_type> matrix = buildMatrix(galeriList, comm);
 
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    *out << "\n>> II. Create the right-hand side vector will all ones.\n" << std::endl;
+    // matrix->describe(*allOut, Teuchos::VERB_EXTREME);
 
-    RCP<vector_type> rhs = rcp(new Tpetra::Vector(matrix->getRangeMap()));
-    rhs->putScalar(one);
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    *out << "\n>> II. Create and empty solution vector and a right-hand side vector will all ones.\n" << std::endl;
+
+    RCP<vec_type> x = rcp(new vec_type(matrix->getDomainMap(), true));
+    RCP<vec_type> rhs = rcp(new vec_type(matrix->getRangeMap(),true));
+    MVT::MvRandom(*x);
+    OPT::Apply(*matrix, *x, *rhs);
+    MVT::MvInit(*x, zero);
+
+    // RCP<vec_type> x = rcp(new vec_type(matrix->getDomainMap(), true));
+    // RCP<vec_type> rhs = rcp(new vec_type(matrix->getRangeMap()));
+    // rhs->putScalar(one);
+
+    x->describe(*allOut, Teuchos::VERB_EXTREME);
+    rhs->describe(*allOut, Teuchos::VERB_EXTREME);
 
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     *out << "\n>> III. Create a CG solver from the Belos package.\n" << std::endl;
 
-    const int numGlobalElements = B->getGlobalLength();
-    if (maxiters == -1) {
-      maxiters = numGlobalElements - 1; // maximum number of iterations to run
+    const int numGlobalElements = rhs->getGlobalLength();
+    if (maxIters == -1) {
+      maxIters = numGlobalElements - 1; // maximum number of iterations to run
     }
     //
-    ParameterList belosList;
-    belosList.set( "Maximum Iterations", maxiters);       // Maximum number of iterations allowed
+    Teuchos::ParameterList belosList;
+    belosList.set( "Maximum Iterations", maxIters);       // Maximum number of iterations allowed
     belosList.set( "Convergence Tolerance", tol);         // Relative convergence tolerance requested
     belosList.set( "Num Blocks", 100);                    // Maximum number of blocks in Krylov subspace (max subspace size)
     belosList.set( "Flexible Gmres", false);              // Do not use FGMRES
 
     int verbLevel = Belos::Errors + Belos::Warnings;
-    if (debug) {
-      verbLevel += Belos::Debug;
-    }
-    if (verbose) {
-      verbLevel += Belos::TimingDetails + Belos::FinalSummary + Belos::StatusTestDetails;
-    }
-    belosList.set( "Verbosity", verbLevel );
-    if (verbose) {
-      if (frequency > 0) {
-        belosList.set( "Output Frequency", frequency );
-      }
-    }
+    // if (debug) {
+    //   verbLevel += Belos::Debug;
+    // }
+    // if (verbose) {
+    //   verbLevel += Belos::TimingDetails + Belos::FinalSummary + Belos::StatusTestDetails;
+    // }
+    // belosList.set( "Verbosity", verbLevel );
+    // if (verbose) {
+    //   if (frequency > 0) {
+    //     belosList.set( "Output Frequency", frequency );
+    //   }
+    // }
     // Construct an unpreconditioned linear problem instance.
     //
-    Belos::LinearProblem<ST,MV,OP> problem( A, X, B );
-    problem.setInitResVec( B );
+    Belos::LinearProblem<scalar_type, multivec_type, operator_type> problem(matrix, x, rhs);
+    problem.setInitResVec(rhs);
     bool set = problem.setProblem();
     if (set == false) {
-      if (proc_verbose)
-        std::cout << std::endl << "ERROR:  Belos::LinearProblem failed to set up correctly!" << std::endl;
+      *out << std::endl << "ERROR:  Belos::LinearProblem failed to set up correctly!" << std::endl;
       return -1;
     }
 
-    Belos::BlockGmresSolMgr<ST,MV,OP> solver(rcpFromRef(problem), rcpFromRef(belosList));
+    Belos::BlockCGSolMgr<scalar_type, multivec_type, operator_type> solver(Teuchos::rcpFromRef(problem), Teuchos::rcpFromRef(belosList));
 
-    if (proc_verbose) {
-      std::cout << std::endl << std::endl;
-      std::cout << "Dimension of matrix: " << NumGlobalElements << std::endl;
-      std::cout << "Number of right-hand sides: " << numrhs << std::endl;
-      std::cout << "Block size used by solver: " << blocksize << std::endl;
-      std::cout << "Max number of Gmres iterations: " << maxiters << std::endl;
-      std::cout << "Relative residual tolerance: " << tol << std::endl;
-      std::cout << std::endl;
-    }
+    // RCP<Belos::SolverManager<scalar_type, multivec_type, operator_type> > solver;
+    // Belos::SolverFactory<scalar_type, multivec_type, operator_type> factory;
+    // solver = factory.create ("Block CG", Teuchos::rcpFromRef(belosList));
+
+    // *out << "\nDimension of matrix: " << numGlobalElements << "\n";
+    // *out << "Max number of Gmres iterations: " << maxIters << "\n";
+    // *out << "Relative residual tolerance: " << tol << "\n";
+    // *out << std::endl;
 
     Belos::ReturnType ret = solver.solve();
+    if (ret == Belos::Unconverged)
+    {
+      *out << "Belos did not converge in " << solver.getNumIters() << " iterations." << std::endl;
+      return -1;
+    }
 
     return 0;
   }
--- exercises/ex_03_solve/src/utils.hpp
+++ exercises/ex_03_solve/src/utils.hpp
@@ -12,27 +12,37 @@
 
 #include <Tpetra_CrsMatrix.hpp>
 
+#include <Xpetra_CrsMatrix.hpp>
 #include <Xpetra_CrsMatrixWrap.hpp>
 #include <Xpetra_Map.hpp>
 #include <Xpetra_MapFactory.hpp>
+#include <Xpetra_Matrix.hpp>
 #include <Xpetra_MultiVector.hpp>
 #include <Xpetra_TpetraCrsMatrix.hpp>
+
+using Scalar = Tpetra::CrsMatrix<>::scalar_type;
+using LocalOrdinal = Tpetra::CrsMatrix<>::local_ordinal_type;
+using GlobalOrdinal = Tpetra::CrsMatrix<>::global_ordinal_type;
+using Node = Tpetra::CrsMatrix<>::node_type;
+
+using Teuchos::RCP;
 
 RCP<const Tpetra::CrsMatrix<Scalar,LocalOrdinal,GlobalOrdinal,Node>>
 buildMatrix(Teuchos::ParameterList& matrixList, RCP<const Teuchos::Comm<int>> comm)
 {
-  using Teuchos::RCP;
-
-  using XCrsMatrixWrap = Xpetra::CrsMatrixWrap<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
+  // using XCrsMatrix = Xpetra::CrsMatrix<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
+  using XTeptraCrsMatrix = Xpetra::TpetraCrsMatrix<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
+  // using XCrsMatrixWrap = Xpetra::CrsMatrixWrap<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
   using XMap = Xpetra::Map<LocalOrdinal,GlobalOrdinal,Node>;
   using XMapFactory = Xpetra::MapFactory<LocalOrdinal,GlobalOrdinal,Node>;
+  // using XMatrix = Xpetra::Matrix<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
   using XMultiVector = Xpetra::MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node>;
 
   using crs_matrix_type = Tpetra::CrsMatrix<>;
 
-  Xpetra::UnderlyingLib lib = Xpetra::Tpetra;
+  Xpetra::UnderlyingLib lib = Xpetra::UseTpetra ;
 
-  const global_ordinal_type zero = Teuchos::ScalarTraits<GlobalOrdinal>::zero();
+  const GlobalOrdinal zero = Teuchos::ScalarTraits<GlobalOrdinal>::zero();
 
   GlobalOrdinal nx,ny,nz;
   nx = ny = nz = Teuchos::as<GlobalOrdinal>(5);
@@ -53,12 +63,12 @@ buildMatrix(Teuchos::ParameterList& matrixList, RCP<const Teuchos::Comm<int>> co
   else if  (matrixType == "Elasticity3D")
     numGlobalElements = 3 * nx * ny * nz;
   else
-    TEUCHOS_ASSERT(false, "Unkonwn matrix type" << matrixType.c_str());;
+    	TEUCHOS_ASSERT(false);
 
   RCP<const XMap> map = XMapFactory::Build(lib, numGlobalElements, 0, comm);
-  RCP<Galeri::Xpetra::Problem<XMap,XCrsMatrixWrap,XMultiVector> > problem =
-    Galeri::Xpetra::BuildProblem<Scalar,LocalOrdinal,GlobalOrdinal,XMap,XCrsMatrixWrap,XMultiVector>(matrixType, map, matrixList);
-  RCP<XMatrix> xpetraMatrix = problem->BuildMatrix();
+  RCP<Galeri::Xpetra::Problem<XMap,XTeptraCrsMatrix,XMultiVector> > problem =
+    Galeri::Xpetra::BuildProblem<Scalar,LocalOrdinal,GlobalOrdinal,XMap,XTeptraCrsMatrix,XMultiVector>(matrixType, map, matrixList);
+  RCP<XTeptraCrsMatrix> tpetraCrsMatrix = problem->BuildMatrix();
 
-  return xpetraMatrix->getTpetra_CrsMatrix();
+  return tpetraCrsMatrix->getTpetra_CrsMatrix();
 }
